b92b80a77d39904111c448cc119d85927396e2d3
Emin <185828471+3m1n17@users.noreply.github.com>
2025-05-09 01:26:40 +0300

    Add files via upload

---
+++ MethodsExample.cs
@@ -0,0 +1,16 @@
+﻿/*MethodExample*/
+using System;
+class MethodExample
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        int result = Add(5, 10);
+        Console.WriteLine("The sum is: " + result);
+    }
+    static int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+
