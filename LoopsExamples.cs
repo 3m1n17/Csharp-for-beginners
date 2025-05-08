f819ae3bcc743f8073a7d6f3d879f4b118d401ba
Emin <185828471+3m1n17@users.noreply.github.com>
2025-05-09 01:18:41 +0300

    Add files via upload

---
+++ LoopsExamples.cs
@@ -0,0 +1,39 @@
+﻿/*For loops*/
+using System;
+class Forloop
+{
+    static void Main(string[] args)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
+
+/*While loops*/
+class Whileloop
+{
+    static void Main(string[] args)
+    {
+        int i = 0;
+        while (i < 10)
+        {
+            Console.WriteLine(i);
+            i++;
+        }
+    }
+}
+/*Do while loops*/
+class Dowhileloop
+{
+    static void Main(string[] args)
+    {
+        int i = 0;
+        do
+        {
+            Console.WriteLine(i);
+            i++;
+        } while (i < 10);
+    }
+}
